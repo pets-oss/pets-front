05fc0a17b795ea71db033aef1714747098211bd5
Saulius Vikerta <svikerta@kayak.com>
2020-11-30 11:14:42 +0200

    fixing test

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Pets information system</h1>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.<br />
           Happy coding!
