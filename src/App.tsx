ca46626d631e2e713177074a682a2ce6322e7d78
Benas Nekrošius <benasnek@gmail.com>
2020-11-30 14:49:15 +0200

    Setup Material-UI. #6 (#11)
    * Setup Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import logo from "./logo.svg";
+import theme from "./theme";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Pets information system</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.<br />
-          Happy coding!
-        </p>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Typography variant="h3" component="h1">
+            Pets information system
+          </Typography>
+          <Typography variant="body1">
+            Edit <code>src/App.tsx</code> and save to reload.
+            <br />
+            Happy coding!
+          </Typography>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#ffd6c9",
+      main: "#d1a598",
+      dark: "#9f766a",
+    },
+    secondary: {
+      light: "#417565",
+      main: "#13493b",
+      dark: "#002115",
+    },
+  },
+});
+
+export default theme;
