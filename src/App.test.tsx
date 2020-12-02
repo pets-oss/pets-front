fc2a1aced60669dd5765e0dcf2bc2681d1bcd2fe
Saulius Vikerta <svikerta@kayak.com>
2020-12-02 23:08:57 +0200

    updating the demo test message

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders some app content', () => {
   render(<App />);
   const linkElement = screen.getByText(/Pets information system/i);
   expect(linkElement).toBeInTheDocument();
