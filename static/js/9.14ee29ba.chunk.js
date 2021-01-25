005565b409f570d57a106551d707b13c1651cfb3
PPrakapavicius <PPrakapavicius@users.noreply.github.com>
2021-01-25 15:26:25 +0000

    deploy: 4876519b4682610438f351972d5ee66825bf33e0

--- static/js/9.14ee29ba.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppets-front"]=this["webpackJsonppets-front"]||[]).push([[9],{279:function(t,e,n){"use strict";n.r(e);var a=n(4),s=(n(0),n(39)),o=n(60);e.default=function(){return Object(a.jsxs)("div",{className:"NotFound-page",children:[Object(a.jsx)(o.a,{variant:"h3",component:"h1",children:"Page Not Found"}),Object(a.jsxs)(o.a,{variant:"body1",children:["Try navigating to ",Object(a.jsx)(s.b,{to:"/",children:"homepage"})]})]})}}}]);
-//# sourceMappingURL=9.14ee29ba.chunk.js.map
