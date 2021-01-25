78837019826dca380094885dd0591cc0bc483ecc
R. K <rkiupelis@kayak.com>
2021-01-25 16:45:40 +0200

    Update App.tsx
    Removing path for page not found.

--- src/App.tsx
+++ src/App.tsx
@@ -78,8 +78,6 @@ export default function App() {
                             />
                             <Route exact path="/" component={React.lazy(() => import('./pages/Home'))} />
                             <Route
-                                exact
-                                path="/not-found"
                                 component={React.lazy(() => import('./pages/PageNotFound'))}
                             />
                         </React.Suspense>
