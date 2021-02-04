39de7492ecb01bf154b8d581f5c06352b068b2bf
Pavelas Adomaitis <padomaitis@kayak.com>
2021-02-04 19:14:02 +0200

    Fill animal details component with data from graphql

--- src/pages/AnimalDetails.tsx
+++ src/pages/AnimalDetails.tsx
@@ -1,14 +1,24 @@
+import { loader } from 'graphql.macro';
 import Image from 'material-ui-image';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+import { useQuery } from '@apollo/client';
 import { Box, Typography } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Skeleton } from '@material-ui/lab';
 import AnimalEventList from '../components/animal/AnimalEventList';
 import LayoutMultiColRow from '../components/layout/LayoutMultiColRow';
+import { Animal } from '../graphql/types';
+import { getAnimalAge, getAnimalWeight } from '../utils/animal';
 
+const GET_ANIMAL_DETAILS = loader('../graphql/queries/animal-details.graphql');
 interface RouterParams {
     id: string;
+}
+
+interface Response {
+    animal: Animal;
 }
 
 const useStyles = makeStyles(theme =>
@@ -30,21 +40,46 @@ function AnimalDetails() {
     const { id } = params;
     const classes = useStyles();
 
+    const { loading, error, data } = useQuery<Response>(GET_ANIMAL_DETAILS, {
+        variables: { id: Number(id) },
+    });
+
+    if (loading) {
+        return <Skeleton animation="wave" height="70vh" />;
+    }
+
+    if (error) {
+        // TODO: replace with proper UI elements
+        return <p>Error!</p>;
+    }
+
+    if (!data?.animal) {
+        // TODO: replace with proper UI elements
+        return <p>No data!</p>;
+    }
+
+    const { animal } = data;
+    const birthDay = animal.details?.birthDate ? getAnimalAge(animal.details.birthDate) : '';
+
     return (
         <div className={classes.root}>
             <LayoutMultiColRow>
-                <Image src="https://picsum.photos/600/338" aspectRatio={16 / 9} />
+                <Image src={animal.imageUrl!} aspectRatio={16 / 9} />
                 <>
                     <Box mt={1}>
                         <Typography className={classes.animalName} variant="h3" component="h1">
-                            dog Scooby Doo
+                            {animal.name}
                         </Typography>
                     </Box>
-                    <Box mt={1}>
-                        <Typography className={classes.animalMeta} variant="body1">
-                            Breed, gender, weight, color, birth_date
-                        </Typography>
-                    </Box>
+                    {animal.details && (
+                        <Box mt={1}>
+                            <Typography className={classes.animalMeta} variant="body1">
+                                {`${animal.details.breed}, ${animal.details.gender}, ${getAnimalWeight(
+                                    animal.details.weight!
+                                )}, ${animal.details.color}, ${birthDay}`}
+                            </Typography>
+                        </Box>
+                    )}
                     <Box mt={1}>
                         <Typography variant="body1">Referencing Animal ID:{id}</Typography>
                     </Box>
--- src/utils/animal.ts
+++ src/utils/animal.ts
@@ -1,6 +1,9 @@
 import { formatDistance } from 'date-fns';
+import ltLocale from 'date-fns/locale/lt';
 
 import { capitalizeFirstLetter } from './string';
 
-export const getAnimalAge = (birthTimestamp: number): string =>
-    capitalizeFirstLetter(formatDistance(birthTimestamp, new Date()));
+export const getAnimalAge = (birthTimestamp: string): string =>
+    capitalizeFirstLetter(formatDistance(Number(birthTimestamp), new Date(), { locale: ltLocale }));
+
+export const getAnimalWeight = (weight: number): string => (weight ? `${weight}kg` : '');
