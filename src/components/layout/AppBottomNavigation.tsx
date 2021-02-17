134aad16ffd9fb378130a78728ff8db7f3146661
Saulius Vikerta <svikerta@kayak.com>
2021-02-18 00:43:41 +0200

    add AppBottomNavigation

--- src/components/layout/AppBottomNavigation.tsx
+++ src/components/layout/AppBottomNavigation.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
+import { useAuth0 } from '@auth0/auth0-react';
 import AppBar from '@material-ui/core/AppBar';
+import Divider from '@material-ui/core/Divider';
 import Drawer from '@material-ui/core/Drawer';
 import Fab from '@material-ui/core/Fab';
 import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import AddIcon from '@material-ui/icons/Add';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import CloseIcon from '@material-ui/icons/Close';
 import MenuIcon from '@material-ui/icons/Menu';
+import navigation from '../../navigation';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -16,11 +27,13 @@ const useStyles = makeStyles((theme: Theme) =>
             bottom: 0,
             backgroundColor: theme.palette.primary.light,
         },
-        list: {
-            width: 250,
-        },
-        fullList: {
-            width: 'auto',
+        drawerHeader: {
+            display: 'flex',
+            alignItems: 'center',
+            padding: theme.spacing(0, 3),
+            // necessary for height alignment with app bar
+            ...theme.mixins.toolbar,
+            justifyContent: 'space-between',
         },
         fabButton: {
             position: 'absolute',
@@ -33,14 +46,63 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default function AppBottomNavigation() {
     const classes = useStyles();
-    const [drawerIsOpen, setDrawerState] = React.useState(false);
+    const history = useHistory();
+    const location = useLocation();
+    const [open, setDrawerOpen] = React.useState(false);
+    const { isAuthenticated } = useAuth0();
 
-    // todo: the menu list
+    const handleDrawerOpen = () => {
+        setDrawerOpen(true);
+    };
+
+    const handleDrawerClose = () => {
+        setDrawerOpen(false);
+    };
+
+    const handleNavListItemClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, pathTo: string) => {
+        history.push(pathTo);
+        setDrawerOpen(false);
+    };
+
+    const linkToCurrentPage = (pathTo: string) => {
+        return location.pathname === pathTo;
+    };
+
+    const navList = () => (
+        <div role="presentation">
+            <div className={classes.drawerHeader}>
+                <Typography variant="h6">PetBook</Typography>
+                <IconButton edge="end" onClick={handleDrawerClose}>
+                    <CloseIcon />
+                </IconButton>
+            </div>
+            <Divider />
+            <List>
+                {navigation
+                    .filter(item => {
+                        return item.authRequired ? isAuthenticated : true;
+                    })
+                    .map(item => (
+                        <ListItem
+                            button
+                            key={item.title}
+                            selected={linkToCurrentPage(item.to)}
+                            onClick={event => handleNavListItemClick(event, item.to)}
+                        >
+                            <ListItemIcon>
+                                <ChevronRightIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={item.title} />
+                        </ListItem>
+                    ))}
+            </List>
+        </div>
+    );
 
     return (
-        <AppBar position="fixed" color="default" className={classes.appBar}>
+        <AppBar className={classes.appBar} position="fixed" color="default">
             <Toolbar>
-                <IconButton edge="start" onClick={() => setDrawerState(true)} color="inherit" aria-label="open drawer">
+                <IconButton edge="start" onClick={handleDrawerOpen} color="inherit" aria-label="open drawer">
                     <MenuIcon />
                 </IconButton>
                 {/* FAB element should be rendered from the page context */}
@@ -48,8 +110,8 @@ export default function AppBottomNavigation() {
                     <AddIcon />
                 </Fab>
             </Toolbar>
-            <Drawer anchor="bottom" open={drawerIsOpen} onClose={() => setDrawerState(false)}>
-                some content
+            <Drawer anchor="bottom" open={open} onClose={handleDrawerClose}>
+                {navList()}
             </Drawer>
         </AppBar>
     );
