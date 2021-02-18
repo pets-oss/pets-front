93589139e3c3362c913f8c1204655b522af8eabf
rikius <rkiupelis@kayak.com>
2021-02-18 18:20:00 +0200

    - updating GraphQl query - updating access of null type in animalAge.

--- src/utils/animal.ts
+++ src/utils/animal.ts
@@ -3,7 +3,13 @@ import ltLocale from 'date-fns/locale/lt';
 
 import { capitalizeFirstLetter } from './string';
 
-export const getAnimalAge = (birthTimestamp: string): string =>
-    capitalizeFirstLetter(formatDistance(Number(birthTimestamp), new Date(), { locale: ltLocale }));
+export const getAnimalAge = (birthTimestamp: string | null = ''): string => {
+    if (birthTimestamp) {
+        const distance = formatDistance(Number(birthTimestamp), new Date(), { locale: ltLocale });
+        return capitalizeFirstLetter(distance);
+    }
+
+    return '';
+};
 
 export const getAnimalWeight = (weight: number): string => (weight ? `${weight}kg` : '');
