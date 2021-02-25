6f05e9cac55f8109cfbe35158f89eb191094dc15
rikius <rikius@users.noreply.github.com>
2021-02-25 16:59:57 +0000

    deploy: 330ced0bfe16d4ef3778677850ff0625f2e23d49

--- static/js/0.42c5caa8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppets-front"]=this["webpackJsonppets-front"]||[]).push([[0],{324:function(t,e,n){"use strict";n.r(e);var s=n(4),a=(n(0),n(32)),i=n(73);e.default=function(){return Object(s.jsxs)("div",{className:"NotFound-page",children:[Object(s.jsx)(i.a,{variant:"h3",component:"h1",children:"Page is not implemented yet"}),Object(s.jsxs)(i.a,{variant:"body1",children:["Try navigating to ",Object(s.jsx)(a.b,{to:"/",children:"homepage"})]})]})}}}]);
-//# sourceMappingURL=0.42c5caa8.chunk.js.map
