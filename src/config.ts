178b6ebabc156966b9e64921e8a2542ebc8b488d
PPrakapavicius <75241329+PPrakapavicius@users.noreply.github.com>
2021-03-02 11:43:18 +0200

    Add animal and event cards (#60)
    * Add animal and event cards
    * Fix card styles, move logo's base url to config.ts
    
    Co-authored-by: PPrakapvicius <p.prakapavicius@gmail.com>

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,7 @@ interface Config {
     AUTH0_DOMAIN: string;
     AUTH0_CLIENT_ID: string;
     AUTH0_AUDIENCE_URL: string;
+    PUBLIC_URL: string;
 }
 
 const config: Config = {
@@ -10,6 +11,7 @@ const config: Config = {
     AUTH0_DOMAIN: process.env.REACT_APP_AUTH0_DOMAIN!,
     AUTH0_CLIENT_ID: process.env.REACT_APP_AUTH0_CLIENT_ID!,
     AUTH0_AUDIENCE_URL: process.env.REACT_APP_AUTH0_AUTH_AUDIENCE!,
+    PUBLIC_URL: process.env.PUBLIC_URL!,
 };
 
 export default config;
