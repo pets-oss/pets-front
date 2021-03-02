2a902acca90d6623d1dd7cd9fa916b435a077eb9
PPrakapavicius <PPrakapavicius@users.noreply.github.com>
2021-03-02 09:47:29 +0000

    deploy: 178b6ebabc156966b9e64921e8a2542ebc8b488d

---
+++ static/js/0.e2229988.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppets-front"]=this["webpackJsonppets-front"]||[]).push([[0],{333:function(t,e,n){"use strict";n.r(e);var s=n(4),a=(n(0),n(33)),i=n(74);e.default=function(){return Object(s.jsxs)("div",{className:"NotFound-page",children:[Object(s.jsx)(i.a,{variant:"h3",component:"h1",children:"Page is not implemented yet"}),Object(s.jsxs)(i.a,{variant:"body1",children:["Try navigating to ",Object(s.jsx)(a.b,{to:"/",children:"homepage"})]})]})}}}]);
+//# sourceMappingURL=0.e2229988.chunk.js.map
