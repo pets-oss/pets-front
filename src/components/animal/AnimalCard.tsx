a2d419e086fcd1d8f69553c7a44579a0e8c1ed05
Saulius Vikerta <svikerta@kayak.com>
2021-03-03 18:34:02 +0200

    restyle AnimalCard: favorite, header

--- src/components/animal/AnimalCard.tsx
+++ src/components/animal/AnimalCard.tsx
@@ -1,45 +1,65 @@
+import clsx from 'clsx';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import { Card, CardActionArea, CardHeader, CardMedia, IconButton, Typography } from '@material-ui/core';
+import { Box, Card, CardActionArea, CardHeader, CardMedia, IconButton, Typography } from '@material-ui/core';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
-import makeStyles from '@material-ui/core/styles/makeStyles';
+import { makeStyles, Theme } from '@material-ui/core/styles';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import config from '../../config';
 import { Animal } from '../../graphql/types';
+import { getYMDDateFromTS } from '../../utils/dateFormatters';
 
 const VALID_GENDER_IDS = ['1', '2'];
 const VALID_SPECIES_IDS = ['1', '2'];
 
 export default function AnimalCard({ animal }: AnimalCardProps) {
     const classes = useStyles();
+    const [favorite, setFavorite] = React.useState(false);
+    let formatedRegistrationDate;
+    if (animal.registration?.registrationDate) {
+        formatedRegistrationDate = getYMDDateFromTS(animal.registration?.registrationDate);
+    } else {
+        formatedRegistrationDate = 'n/d';
+    }
+
+    // just mocking toggle
+    const handleFavoriteClick = () => {
+        setFavorite(!favorite);
+    };
 
     return (
         <Grid item xs={10} md={6} lg={3}>
             <Card>
-                <CardActionArea component={NavLink} to={`/animal/${animal.id}`}>
-                    <CardMedia
-                        className={classes.media}
-                        image={animal.imageUrl || `https://eu.ui-avatars.com/api/?name=${animal.name}`}
-                        title="Animal picture"
-                    />
-                </CardActionArea>
+                <Box className={classes.cardMediaWrapper}>
+                    <CardActionArea component={NavLink} to={`/animal/${animal.id}`}>
+                        <CardMedia
+                            className={classes.media}
+                            image={animal.imageUrl || `https://eu.ui-avatars.com/api/?name=${animal.name}`}
+                            title="Animal picture"
+                        />
+                    </CardActionArea>
+                    <IconButton
+                        className={clsx(classes.favoriteButton, {
+                            [classes.isFavorite]: favorite,
+                        })}
+                        onClick={handleFavoriteClick}
+                        aria-label="add to favorites"
+                    >
+                        <FavoriteIcon />
+                    </IconButton>
+                </Box>
                 <CardHeader
                     avatar={<Avatar aria-label="specie" alt="specie" src={resolveIconSrc(animal)} />}
-                    action={
-                        <IconButton aria-label="add to favorites">
-                            <FavoriteIcon />
-                        </IconButton>
-                    }
                     title={
-                        <Typography className={classes.headerText} noWrap>
+                        <Typography component="h6" className={classes.headerText} noWrap>
                             {animal.name}
                         </Typography>
                     }
                     subheader={
-                        <Typography className={classes.headerText} noWrap>
-                            {animal.registration?.registrationDate}
+                        <Typography className={classes.subHeaderText} noWrap>
+                            {formatedRegistrationDate}
                         </Typography>
                     }
                 />
@@ -61,13 +81,43 @@ function resolveIconSrc(animal: Animal) {
     return `${config.PUBLIC_URL}/gender${animal.details.gender.id}-species${speciesId}.png`;
 }
 
-const useStyles = makeStyles(() => ({
+const isFavoriteColor = '#D10C0C';
+
+const useStyles = makeStyles((theme: Theme) => ({
+    cardMediaWrapper: {
+        position: 'relative',
+    },
     media: {
         height: 0,
         paddingTop: '56.25%', // 16:9
+        zIndex: 0,
+    },
+    favoriteButton: {
+        position: 'absolute',
+        top: theme.spacing(2),
+        right: theme.spacing(2),
+        zIndex: 1,
+        backgroundColor: 'rgba(255, 255, 255, .3)',
+        '&:hover': {
+            backgroundColor: 'rgba(255, 255, 255, .6)',
+        },
+        '&:focus': {
+            backgroundColor: 'rgba(255, 255, 255, .6)',
+        },
+    },
+    isFavorite: {
+        color: isFavoriteColor,
+        backgroundColor: 'rgba(255, 255, 255, .75)',
     },
     headerText: {
         maxWidth: 150,
+        fontSize: 20,
+        lineHeight: '24px',
+        fontWeight: 600,
+    },
+    subHeaderText: {
+        maxWidth: 150,
+        color: theme.palette.grey['600'],
     },
 }));
 
