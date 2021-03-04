034c5d99c472d8d050c6c42b8387ad3003515fe2
rikius <rkiupelis@kayak.com>
2021-03-04 14:49:11 +0200

    Updating types.

--- src/graphql/data-transform/species.ts
+++ src/graphql/data-transform/species.ts
@@ -1,18 +1,26 @@
 import { Species } from '../types';
 
 export const enum SpeciesType {
-    DOG = '1',
-    CAT = '2',
+    DOG = 'dog',
+    CAT = 'cat',
     UNDEFINED = 'Undefined',
 }
+
+const SpeciesMapList: {
+    [key: string]: SpeciesType;
+} = {
+    '1': SpeciesType.DOG,
+    '2': SpeciesType.CAT,
+};
 
 export default function getSpecieType(species?: Species): SpeciesType {
     if (!species) {
         return SpeciesType.UNDEFINED;
     }
-    // if (species.id in SpeciesType) {
-    //     // return
-    // }
+
+    if (species.id in SpeciesMapList) {
+        return SpeciesMapList[species.id];
+    }
 
     return SpeciesType.UNDEFINED;
 }
