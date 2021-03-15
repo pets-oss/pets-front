43ba3cb23341ee95b70df175d9942e23ded246a0
Saulius Vikerta <svikerta@kayak.com>
2021-03-15 17:35:11 +0200

    add info to About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,56 +1,30 @@
 import React from 'react';
 
-import { Typography } from '@material-ui/core';
+import { Box, Typography } from '@material-ui/core';
 import Page from './Page';
 
 function About() {
     return (
         <Page title="About this project">
             <Typography variant="body1" paragraph>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+                Open Source database for administration and documentation of sheltered animals information.
             </Typography>
             <Typography variant="body1" paragraph>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud ex ercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+                <strong>Goal:</strong> Easen the process of sheltered animals information administration.
             </Typography>
             <Typography variant="body1" paragraph>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+                <strong>Main functions:</strong>
             </Typography>
+            <Box ml={2} mb={2}>
+                <ul>
+                    <li>Portfolios of Animal shelter organizations</li>
+                    <li>Animal cards with main information for shelter/future owners</li>
+                    <li>Animal check-in/check-out process</li>
+                    <li>Various events related to particular animal care - like vacinnation, sterilization etc.</li>
+                </ul>
+            </Box>
             <Typography variant="body1" paragraph>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
-            </Typography>
-            <Typography variant="h1" component="h1">
-                Heading1
-            </Typography>
-            <Typography variant="h2" component="h2">
-                Heading2
-            </Typography>
-            <Typography variant="h3" component="h3">
-                Heading3
-            </Typography>
-            <Typography variant="h4" component="h4">
-                Heading4
-            </Typography>
-            <Typography variant="h5" component="h5">
-                Heading5
-            </Typography>
-            <Typography variant="h6" component="h6">
-                Heading6
+                And many more to come :)
             </Typography>
         </Page>
     );
