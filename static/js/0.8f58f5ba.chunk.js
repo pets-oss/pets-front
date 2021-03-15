7dd81b29887512c4d3e7c12ed5969f7ced782932
benNek <benNek@users.noreply.github.com>
2021-03-15 07:02:46 +0000

    deploy: 7d6be6208a513725ad7118584d9d6d16a348079f

---
+++ static/js/0.8f58f5ba.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppets-front"]=this["webpackJsonppets-front"]||[]).push([[0],{394:function(t,e,n){"use strict";n.r(e);var s=n(4),a=(n(0),n(36)),i=n(80),c=n(130);e.default=function(){return Object(s.jsxs)(c.a,{children:[Object(s.jsx)(i.a,{variant:"h3",component:"h1",children:"Page is not implemented yet"}),Object(s.jsxs)(i.a,{variant:"body1",children:["Try navigating to ",Object(s.jsx)(a.b,{to:"/",children:"homepage"})]})]})}}}]);
+//# sourceMappingURL=0.8f58f5ba.chunk.js.map
