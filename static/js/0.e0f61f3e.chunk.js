7badefb85547cf381b3fc0deafff29b7a8694356
mantasgailius <mantasgailius@users.noreply.github.com>
2021-03-15 16:02:30 +0000

    deploy: 204caffc0d6c6b90701bf0eef924fc1376f35428

---
+++ static/js/0.e0f61f3e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppets-front"]=this["webpackJsonppets-front"]||[]).push([[0],{406:function(t,e,n){"use strict";n.r(e);var s=n(4),a=(n(0),n(36)),i=n(62),c=n(136);e.default=function(){return Object(s.jsxs)(c.a,{children:[Object(s.jsx)(i.a,{variant:"h3",component:"h1",children:"Page is not implemented yet"}),Object(s.jsxs)(i.a,{variant:"body1",children:["Try navigating to ",Object(s.jsx)(a.b,{to:"/",children:"homepage"})]})]})}}}]);
+//# sourceMappingURL=0.e0f61f3e.chunk.js.map
