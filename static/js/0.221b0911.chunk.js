96396e912dac1a7f4490bdd13bf52eafd9228848
saulyz <saulyz@users.noreply.github.com>
2021-03-15 15:51:22 +0000

    deploy: 2c495f1cdcb6aeadb38a2fac787ac0abbe017b7b

--- static/js/0.221b0911.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppets-front"]=this["webpackJsonppets-front"]||[]).push([[0],{397:function(t,e,n){"use strict";n.r(e);var s=n(4),a=(n(0),n(36)),i=n(61),c=n(131);e.default=function(){return Object(s.jsxs)(c.a,{children:[Object(s.jsx)(i.a,{variant:"h3",component:"h1",children:"Page is not implemented yet"}),Object(s.jsxs)(i.a,{variant:"body1",children:["Try navigating to ",Object(s.jsx)(a.b,{to:"/",children:"homepage"})]})]})}}}]);
-//# sourceMappingURL=0.221b0911.chunk.js.map
