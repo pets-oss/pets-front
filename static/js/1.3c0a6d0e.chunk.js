e8ceccbaf3d2e7145fbf772252f18c5095b0bb21
rikius <rikius@users.noreply.github.com>
2021-03-29 13:13:10 +0000

    deploy: 72f0ff4c8b7688cf9d1c668652c06cac31c43eed

---
+++ static/js/1.3c0a6d0e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonppets-front"]=this["webpackJsonppets-front"]||[]).push([[1],{431:function(t,e,n){"use strict";n.r(e);n(0);var s=n(36),a=n(61),i=n(138),c=n(4);e.default=function(){return Object(c.jsxs)(i.a,{children:[Object(c.jsx)(a.a,{variant:"h3",component:"h1",children:"Page is not implemented yet"}),Object(c.jsxs)(a.a,{variant:"body1",children:["Try navigating to ",Object(c.jsx)(s.b,{to:"/",children:"homepage"})]})]})}}}]);
+//# sourceMappingURL=1.3c0a6d0e.chunk.js.map
