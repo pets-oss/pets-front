e5a941e7e085cd00bea30cbc68a65583f545d60b
Aiste Tamosauskiene <aistetamosauskiene@gmail.com>
2021-04-29 11:12:24 +0300

    Initial commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import React from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
@@ -18,9 +19,25 @@ const LocationDisplay = () => {
 
 const useStyles = makeStyles({
     root: {
+        position: 'relative',
         display: 'flex',
         flexDirection: 'column',
         minHeight: '100vh',
+    },
+    fadeEnter: {
+        opacity: 0,
+        zIndex: 1,
+    },
+    fadeEnterActive: {
+        opacity: 1,
+        transition: 'opacity 300ms linear 150ms',
+    },
+    fadeExit: {
+        opacity: 1,
+    },
+    fadeExitActive: {
+        opacity: 0,
+        transition: 'opacity 150ms linear',
     },
 });
 
@@ -33,42 +50,69 @@ export default function App() {
             <div className={classes.root}>
                 <ResponsiveAppTop />
                 <React.Suspense fallback={<Skeleton variant="rect" height="100vh" />}>
-                    <Switch>
-                        <Route exact path="/about" component={React.lazy(() => import('./pages/About'))} />
-                        <PrivateRoute
-                            exact
-                            path="/user-profile"
-                            component={React.lazy(() => import('./pages/UserProfile'))}
-                        />
-                        <PrivateRoute
-                            exact
-                            path="/animal-list"
-                            component={React.lazy(() => import('./pages/AnimalsPage'))}
-                        />
-                        <PrivateRoute
-                            exact
-                            path="/animal/new"
-                            component={React.lazy(() => import('./pages/NewAnimalPage'))}
-                        />
-                        <PrivateRoute
-                            exact
-                            path="/animal/:id"
-                            component={React.lazy(() => import('./pages/AnimalDetailsPage'))}
-                        />
-                        <Route
-                            exact
-                            path="/search"
-                            component={React.lazy(() => import('./pages/PageNotImplemented'))}
-                        />
-                        <Route exact path="/login" component={React.lazy(() => import('./pages/PageNotImplemented'))} />
-                        <Route
-                            exact
-                            path="/logout"
-                            component={React.lazy(() => import('./pages/PageNotImplemented'))}
-                        />
-                        <Route exact path="/" component={React.lazy(() => import('./pages/Home'))} />
-                        <Route component={PageNotFound} />
-                    </Switch>
+                    <Route
+                        render={({ location }) => (
+                            <TransitionGroup>
+                                <CSSTransition
+                                    key={location.key}
+                                    timeout={200}
+                                    classNames={{
+                                        appear: classes.fadeEnter,
+                                        appearActive: classes.fadeEnterActive,
+                                        enter: classes.fadeEnter,
+                                        enterActive: classes.fadeEnterActive,
+                                        exit: classes.fadeExit,
+                                        exitActive: classes.fadeExitActive,
+                                    }}
+                                >
+                                    <Switch location={location}>
+                                        <Route
+                                            exact
+                                            path="/about"
+                                            component={React.lazy(() => import('./pages/About'))}
+                                        />
+                                        <PrivateRoute
+                                            exact
+                                            path="/user-profile"
+                                            component={React.lazy(() => import('./pages/UserProfile'))}
+                                        />
+                                        <PrivateRoute
+                                            exact
+                                            path="/animal-list"
+                                            component={React.lazy(() => import('./pages/AnimalsPage'))}
+                                        />
+                                        <PrivateRoute
+                                            exact
+                                            path="/animal/new"
+                                            component={React.lazy(() => import('./pages/NewAnimalPage'))}
+                                        />
+                                        <PrivateRoute
+                                            exact
+                                            path="/animal/:id"
+                                            component={React.lazy(() => import('./pages/AnimalDetailsPage'))}
+                                        />
+                                        <Route
+                                            exact
+                                            path="/search"
+                                            component={React.lazy(() => import('./pages/PageNotImplemented'))}
+                                        />
+                                        <Route
+                                            exact
+                                            path="/login"
+                                            component={React.lazy(() => import('./pages/PageNotImplemented'))}
+                                        />
+                                        <Route
+                                            exact
+                                            path="/logout"
+                                            component={React.lazy(() => import('./pages/PageNotImplemented'))}
+                                        />
+                                        <Route exact path="/" component={React.lazy(() => import('./pages/Home'))} />
+                                        <Route component={PageNotFound} />
+                                    </Switch>
+                                </CSSTransition>
+                            </TransitionGroup>
+                        )}
+                    />
                 </React.Suspense>
                 <ResponsiveAppBottom />
             </div>
