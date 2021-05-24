852bd4fc297bfd00fc0a1d480e1c01805fb404b0
Lukas Krisikaitis <lukaskrisikaitis@gmail.com>
2021-05-24 17:22:23 +0300

    Added responsiveness to AnimalDetailsHeader component. (#178)

--- src/components/animal/details/AnimalDetailsHeader.tsx
+++ src/components/animal/details/AnimalDetailsHeader.tsx
@@ -3,39 +3,49 @@ import React from 'react';
 import { Box, IconButton, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function AnimalDetailsHeader({ animalName, gender, species, color, breed, onBack }: AnimalDetailsHeaderProps) {
     const classes = useStyles();
 
     return (
-        <Box display="flex" alignItems="center" flexDirection="row" mb={3}>
-            <IconButton className={classes.backButton} onClick={onBack}>
-                <ArrowBackIcon className={classes.backIcon} />
-            </IconButton>
-            <Box ml={3}>
-                {animalName && <Typography variant="h3">{animalName}</Typography>}
-                <Box display="flex" mt={1}>
-                    {species && (
-                        <Typography variant="body2" className={classes.property}>
-                            {species}
-                        </Typography>
-                    )}
-                    {gender && (
-                        <Typography variant="body2" className={classes.property}>
-                            {gender}
-                        </Typography>
-                    )}
-                    {color && (
-                        <Typography variant="body2" className={classes.property}>
-                            {color}
-                        </Typography>
-                    )}
-                    {breed && (
-                        <Typography variant="body2" className={classes.property}>
-                            {breed}
+        <Box>
+            <Box display="flex" alignItems="center">
+                <IconButton className={classes.backButton} onClick={onBack}>
+                    <ArrowBackIcon className={classes.backIcon} />
+                </IconButton>
+                <Box display="flex" flexGrow="1" flexShrink="1" flexBasis="auto">
+                    {animalName && (
+                        <Typography variant="h3" className={classes.innerName} noWrap>
+                            {animalName}
                         </Typography>
                     )}
                 </Box>
+                <IconButton aria-label="settings">
+                    <MoreVertIcon className={classes.moreVertIcon} />
+                </IconButton>
+            </Box>
+            <Box display="flex" flexWrap="wrap" className={classes.mainParams}>
+                {species && (
+                    <Typography variant="body2" className={classes.property}>
+                        {species}
+                    </Typography>
+                )}
+                {gender && (
+                    <Typography variant="body2" className={classes.property}>
+                        {gender}
+                    </Typography>
+                )}
+                {color && (
+                    <Typography variant="body2" className={classes.property}>
+                        {color}
+                    </Typography>
+                )}
+                {breed && (
+                    <Typography variant="body2" className={classes.property}>
+                        {breed}
+                    </Typography>
+                )}
             </Box>
         </Box>
     );
@@ -44,31 +54,62 @@ function AnimalDetailsHeader({ animalName, gender, species, color, breed, onBack
 export default AnimalDetailsHeader;
 
 const useStyles = makeStyles(theme => ({
-    property: {
-        '&:not(:last-child)': {
-            paddingRight: theme.spacing(2),
-            marginRight: theme.spacing(2),
-            position: 'relative',
-            '&::after': {
-                content: '""',
-                display: 'block',
-                position: 'absolute',
-                height: '100%',
-                width: '2px',
-                backgroundColor: theme.palette.tertiary.dark,
-                top: 0,
-                right: 0,
-            },
-        },
-    },
     backButton: {
         backgroundColor: theme.palette.primary.main,
         '&:hover,&:focus': {
             backgroundColor: theme.palette.primary.dark,
         },
+        marginRight: theme.spacing(2),
+        height: '36px',
+        width: '36px',
+        [theme.breakpoints.up('md')]: {
+            height: '48px',
+            width: '48px',
+        },
     },
     backIcon: {
         color: theme.palette.tertiary.light,
+    },
+    innerName: {
+        fontWeight: 500,
+        maxWidth: 180,
+        [theme.breakpoints.up('md')]: {
+            maxWidth: 340,
+        },
+        [theme.breakpoints.up('lg')]: {
+            maxWidth: 480,
+        },
+    },
+    moreVertIcon: {
+        color: '#202020',
+    },
+    mainParams: {
+        padding: '8px 0',
+        fontSize: '12px',
+        [theme.breakpoints.up('md')]: {
+            padding: '12px 0',
+            fontSize: '14px',
+        },
+    },
+    property: {
+        color: '#666666',
+        fontSize: '12px',
+        [theme.breakpoints.up('md')]: {
+            fontSize: '14px',
+        },
+        '&:not(:last-child)': {
+            paddingRight: theme.spacing(2),
+            marginRight: theme.spacing(2),
+            position: 'relative',
+            '&::after': {
+                color: '#999999',
+                content: '"/"',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 0,
+            },
+        },
     },
 }));
 
