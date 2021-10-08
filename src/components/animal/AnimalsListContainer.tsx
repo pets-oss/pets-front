c169d0fb080416500ce3bbef49172c1a009c3b34
Saulius Vikerta <svikerta@kayak.com>
2021-10-08 12:56:01 +0300

    enlarge size of list page

--- src/components/animal/AnimalsListContainer.tsx
+++ src/components/animal/AnimalsListContainer.tsx
@@ -1,41 +1,57 @@
-import { loader } from 'graphql.macro';
 import React, { useEffect, useState } from 'react';
+import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
-import { useQuery } from '@apollo/client';
 import Skeleton from '@material-ui/lab/Skeleton';
-import { AnimalsConnection } from '../../graphql/types';
-import AnimalsList from './AnimalsList';
+import { Animal, QueryAnimalsArgs } from '../../graphql/types';
+import { fetchAnimals, fetchAnimalsIfNewContext } from '../../store/animalsAll';
+import AnimalCardList from './AnimalCardList';
 import AnimalsTable from './AnimalsTable';
 import PaginationRounded from './PaginationRounded';
 import { AnimalsViewType } from './ViewSelector';
 
-const GET_ANIMALS_QUERY = loader('../../graphql/queries/animal-list.graphql');
 const DEFAULT_PAGE_SIZE = 12;
-
-interface Response {
-    animals: AnimalsConnection;
-}
-
 interface AnimalsListContainerProps {
     viewType: AnimalsViewType;
     setAnimalsCount: (value: number) => void;
 }
 
 export default function AnimalsListContainer({ viewType, setAnimalsCount }: AnimalsListContainerProps) {
-    const [pageSize, setPageSize] = useState(DEFAULT_PAGE_SIZE);
+    const { pathname } = useLocation();
 
-    const { loading, error, data, fetchMore } = useQuery<Response>(GET_ANIMALS_QUERY, {
-        variables: {
-            first: pageSize,
-        },
-    });
+    const [pageSize, setPageSize] = useState(DEFAULT_PAGE_SIZE);
+    const [currentPage, setCurrentPage] = useState(0);
+    const dispatch = useDispatch();
+
+    const { page, isLoading, error } = useSelector((state: RootStateOrAny) => state.animalsAll);
+    const animalObjs: Animal[] = page.objs;
+
+    function filterArgs(args: QueryAnimalsArgs): QueryAnimalsArgs {
+        if (pathname === '/favorites') {
+            return { isFavoriteOnly: true, ...args };
+        }
+        return args;
+    }
 
     useEffect(() => {
-        setAnimalsCount(data?.animals.pageInfo.totalCount ?? 0);
-    }, [setAnimalsCount, data?.animals.pageInfo.totalCount]);
+        dispatch(
+            fetchAnimalsIfNewContext(
+                filterArgs({
+                    first: pageSize,
+                    after: '',
+                }),
+                pathname
+            )
+        );
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [pageSize]);
 
-    if (loading) {
-        return <Skeleton animation="wave" variant="rect" height={500} />;
+    useEffect(() => {
+        setAnimalsCount(page.info.totalCount ?? 0);
+    }, [setAnimalsCount, page.info.totalCount]);
+
+    if (isLoading) {
+        return <Skeleton animation="wave" variant="rect" height={500} width="100%" />;
     }
 
     if (error) {
@@ -43,54 +59,74 @@ export default function AnimalsListContainer({ viewType, setAnimalsCount }: Anim
         return <p>Error!</p>;
     }
 
-    if (!data?.animals?.edges.length) {
+    if (page.info.totalCount === 0) {
         // TODO: replace with proper UI elements
         return <p>No data</p>;
     }
 
-    function loadNextPage() {
-        fetchMore({
-            variables: {
-                first: pageSize,
-                after: data?.animals.pageInfo.endCursor,
-            },
-        });
+    function loadFirstPage(first: number) {
+        dispatch(
+            fetchAnimals(
+                filterArgs({
+                    first,
+                    after: '',
+                })
+            )
+        );
     }
 
-    function loadFirstPage(first: number) {
-        fetchMore({
-            variables: {
-                first,
-                after: '',
-            },
-        });
+    function loadNextPage() {
+        dispatch(
+            fetchAnimals(
+                filterArgs({
+                    first: pageSize,
+                    after: page.info.endCursor,
+                })
+            )
+        );
     }
 
     function loadPreviousPage() {
-        fetchMore({
-            variables: {
-                first: undefined,
-                after: undefined,
-                last: pageSize,
-                before: data?.animals.pageInfo.startCursor,
-            },
-        });
+        dispatch(
+            fetchAnimals(
+                filterArgs({
+                    first: undefined,
+                    after: undefined,
+                    last: pageSize,
+                    before: page.info.startCursor,
+                })
+            )
+        );
+    }
+
+    function handlePageSizeChange(size) {
+        setCurrentPage(0);
+        setPageSize(size);
+        loadFirstPage(size);
+    }
+
+    function handlePageChange(newPage) {
+        if (newPage > currentPage) {
+            loadNextPage();
+        } else {
+            loadPreviousPage();
+        }
+        setCurrentPage(newPage);
     }
 
     return (
         <>
             {viewType === AnimalsViewType.TABLE ? (
-                <AnimalsTable animals={data.animals.edges} />
+                <AnimalsTable animals={animalObjs} />
             ) : (
-                <AnimalsList animals={data.animals.edges} />
+                <AnimalCardList animals={animalObjs} />
             )}
             <PaginationRounded
-                count={data?.animals.pageInfo.totalCount ?? 0}
-                nextPage={loadNextPage}
-                prevPage={loadPreviousPage}
-                firstPage={loadFirstPage}
+                count={page.info.totalCount ?? 0}
+                page={currentPage}
+                onPageChange={handlePageChange}
                 pageSize={pageSize}
-                onPageSizeChange={setPageSize}
+                onPageSizeChange={handlePageSizeChange}
             />
         </>
     );
