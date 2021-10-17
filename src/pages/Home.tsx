032ffb5f325f4c085c2c5f4b1d32f633d38c9d58
Saulius Vikerta <svikerta@kayak.com>
2021-10-17 20:57:07 +0300

    temporary disable Homepage widgets

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
 
-import { useAuth0 } from '@auth0/auth0-react';
+// import { useAuth0 } from '@auth0/auth0-react';
 import { Fade } from '@material-ui/core';
 import AppLogoHeader from '../components/layout/AppLogoHeader';
-import RecentAnimalsContainer from '../components/layout/RecentAnimalsContainer';
-import RecentEventsContainer from '../components/layout/RecentEventsContainer';
 import Page from './Page';
 
 function Home() {
-    const { isAuthenticated } = useAuth0();
+    // const { isAuthenticated } = useAuth0();
 
     return (
         <Fade in timeout={600}>
             <Page>
                 <AppLogoHeader />
-                {isAuthenticated && (
+                {/* {isAuthenticated && (
                     <>
                         <RecentAnimalsContainer />
                         <RecentEventsContainer />
                     </>
-                )}
+                )} */}
             </Page>
         </Fade>
     );
