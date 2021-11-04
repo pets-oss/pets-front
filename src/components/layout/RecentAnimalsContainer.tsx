d0a0876e88c137be7aa0dd9de61d1a91027af58b
Viktorija Kurauskiene <viktorija.sinkeviciute@gmail.com>
2021-11-04 16:12:04 +0200

    Fix RecentAnimalsContainer and RecentEventsContainer in Homepage, closes #206

--- src/components/layout/RecentAnimalsContainer.tsx
+++ src/components/layout/RecentAnimalsContainer.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 import { useQuery } from '@apollo/client';
 import Skeleton from '@material-ui/lab/Skeleton';
-import { Animal } from '../../graphql/types';
+import { Animal, AnimalsConnection } from '../../graphql/types';
 import { sortAnimalsByDate } from '../../utils/animal';
 import AnimalCard from '../animal/AnimalCard';
 import HomePageCarouselContainer from './HomePageCarouselContainer';
 
 const GET_ANIMALS_QUERY = loader('../../graphql/queries/animal-list.graphql');
 interface Response {
-    animals: Animal[];
+    animals: AnimalsConnection;
 }
 
 export default function RecentAnimalsContainer() {
@@ -22,12 +22,12 @@ export default function RecentAnimalsContainer() {
         // TODO: replace with proper UI elements
         return <p>Error!</p>;
     }
-    if (!data?.animals.length) {
+    if (!data?.animals?.edges?.length) {
         // TODO: replace with proper UI elements
         return <p>No data</p>;
     }
 
-    const animals = [...data.animals].sort(sortAnimalsByDate);
+    const animals = data.animals.edges.map(item => item.node as Animal).sort(sortAnimalsByDate);
     const items = animals.map(animal => <AnimalCard animal={animal} key={animal.id} xs={12} md={4} lg={4} />);
 
     return <HomePageCarouselContainer title="Recent animals" itemsToRender={items} />;
